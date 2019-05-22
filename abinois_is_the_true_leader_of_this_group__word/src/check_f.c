737105c46fd00ca5e06df1f5283f914e41fe9629
edillenb <elio.dillenberg@gmail.com>
2019-05-22 19:58:15 +0200

    check_f

--- abinois_is_the_true_leader_of_this_group__word/src/check_f.c
+++ abinois_is_the_true_leader_of_this_group__word/src/check_f.c
@@ -6,7 +6,7 @@
 /*   By: edillenb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 16:39:36 by edillenb          #+#    #+#             */
-/*   Updated: 2019/05/22 18:19:42 by edillenb         ###   ########.fr       */
+/*   Updated: 2019/05/22 19:57:45 by edillenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ long double	check_f_flagz(t_flag flagz, va_list ap)
 		return ((number = va_arg(ap, long double)));
 	return ((number = va_arg(ap, double)));
 }
-
+/*
 char	*ldbltobinary(long double dbl)
 {
 	uint8_t		*nb;
@@ -58,4 +58,46 @@ char	*ldbltobinary(long double dbl)
 	}
 	ft_putstrclr(binary, "blue");
 	return (binary);
+}
+*/
+char	*ldbltobinary(long double dbl)
+{
+	uint8_t		*nb;
+	char		*binary;
+	int			i;
+	size_t		size;
+	uint8_t		mask;
+
+	nb = (uint8_t*)&dbl;
+	i = 0;
+	size = 65;
+	if (!(binary = (char *)malloc(sizeof(char) * size)))
+		return (NULL);
+	binary[--size] = '\0';
+	while (i < 8)
+	{
+		mask = 1;
+		while (mask)
+		{
+			binary[--size] = mask & *nb ? 1 + '0' : 0 + '0';
+			mask <<= 1;
+		}
+		i++;
+		nb++;
+	}
+	ft_putstrclr(binary, "blue");
+	return (binary);
+}
+
+uint16_t	get_exponent(long double dbl)
+{
+	uint16_t	mask;
+	uint16_t	*nb;
+
+	mask = 32767;
+	nb = (uint16_t*)&dbl;
+	nb += 8;
+	nb &= mask;
+	nb -= 16383;
+	return (nb);
 }
