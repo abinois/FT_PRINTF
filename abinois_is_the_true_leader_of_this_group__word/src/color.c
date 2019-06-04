cc9e7ea7a13f51973dc7f9c2cc4a55ca6d45907d
Arthur BINOIS <abinois@e3r10p15.42.fr>
2019-06-04 10:38:11 +0200

    color and underline

--- abinois_is_the_true_leader_of_this_group__word/src/color.c
+++ abinois_is_the_true_leader_of_this_group__word/src/color.c
@@ -6,7 +6,7 @@
 /*   By: abinois <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/26 14:11:19 by abinois           #+#    #+#             */
-/*   Updated: 2019/04/26 14:11:40 by abinois          ###   ########.fr       */
+/*   Updated: 2019/06/04 10:37:03 by abinois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,30 +23,32 @@ void	fill_colors(char **color)
 	color[4] = "{magenta}";
 	color[5] = "{cyan}";
 	color[6] = "{black}";
-	color[7] = "{white}";
-	color[8] = "error";
-	color[9] = "\033[31m";
-	color[10] = "\033[32m";
-	color[11] = "\033[33m";
-	color[12] = "\033[34m";
-	color[13] = "\033[35m";
-	color[14] = "\033[36m";
-	color[15] = "\033[30m";
-	color[16] = "\033[0m";
+	color[7] = "{under}";
+	color[8] = "{reset}";
+	color[9] = "error";
+	color[10] = "\033[31m";
+	color[11] = "\033[32m";
+	color[12] = "\033[33m";
+	color[13] = "\033[34m";
+	color[14] = "\033[35m";
+	color[15] = "\033[36m";
+	color[16] = "\033[30m";
+	color[17] = "\033[4m";
+	color[18] = "\033[0m";
 }
 
 int		what_color(char *buf, int i)
 {
 	int		x;
 	int		a;
-	char	*color[17];
+	char	*color[19];
 
 	fill_colors(color);
 	x = i;
 	a = 0;
-	while (a <= 7 && ft_strncmp(color[a], buf + x, ft_strlen(color[a])))
+	while (a <= 8 && ft_strncmp(color[a], buf + x, ft_strlen(color[a])))
 		a++;
-	return (a == 8 ? -1 : a);
+	return (a == 9 ? -1 : a);
 }
 
 int		return_value(char *buf)
@@ -72,7 +74,7 @@ void	put_color_printf(char *buf, int x)
 	int		i;
 	int		k;
 	int		c;
-	char	*color[17];
+	char	*color[19];
 
 	fill_colors(color);
 	i = -1;
@@ -86,7 +88,7 @@ void	put_color_printf(char *buf, int x)
 		i += k;
 		if (i < x && buf[i] == '{' && (c = what_color(buf, i)) >= 0)
 		{
-			ft_putstr(color[c + 9]);
+			ft_putstr(color[c + 10]);
 			while (buf[i] != '}')
 				i++;
 		}
--- abinois_is_the_true_leader_of_this_group__word/src/main.c
+++ abinois_is_the_true_leader_of_this_group__word/src/main.c
@@ -6,7 +6,7 @@
 /*   By: edillenb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 20:11:36 by edillenb          #+#    #+#             */
-/*   Updated: 2019/06/03 11:52:42 by edillenb         ###   ########.fr       */
+/*   Updated: 2019/06/04 10:37:13 by abinois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,24 +26,22 @@ int		main(int argc, char **argv)
 	//a = 9.99;
 //	b = 0;
 	ft_putstr("____ ftprintf ____\n");
-	rv = ft_printf("{yellow}joli %s{white}\n", "haha");
+	rv = ft_printf("{yellow}joli %s{reset}\n", "haha");
 	printf("Return value = %d\n", rv); 
-	rv = ft_printf("{red}joli %s{white}\n", "haha");
+	rv = ft_printf("{red}joli %s{reset}\n", "haha");
 	printf("Return value = %d\n", rv); 
-	rv = ft_printf("{blue}joli %s{white}\n", "haha");
+	rv = ft_printf("{blue}joli %s{reset}\n", "haha");
 	printf("Return value = %d\n", rv); 
-	rv = ft_printf("{green}joli %s{white}\n", "haha");
+	rv = ft_printf("{green}joli %s{reset}\n", "haha");
 	printf("Return value = %d\n", rv); 
-	rv = ft_printf("{cyan}joli %s{white}\n", "haha");
+	rv = ft_printf("{cyan}joli %s{reset}\n", "haha");
 	printf("Return value = %d\n", rv); 
-	rv = ft_printf("{magenta}joli %s{white}\n", "haha");
+	rv = ft_printf("{magenta}joli %s{reset}\n", "haha");
 	printf("Return value = %d\n", rv); 
-	rv = ft_printf("{black}joli %s{white}\n", "haha");
+	rv = ft_printf("{black}joli %s{reset}\n", "haha");
 	printf("Return value = %d\n", rv); 
-//	rv = ft_printf("{nonvalid1}joli %s{white}\n", "haha");
-//	printf("Return value = %d\n", rv); 
-//	rv = ft_printf("{nonvalid12joli %s{white}\n", "haha");
-//	printf("Return value = %d\n", rv); 
+	rv = ft_printf("{under}joli %s{reset}\n", "haha");
+	printf("Return value = %d\n", rv); 
 	ft_putstr("_____ printf _____\n");
 	rv = printf("joli %s\n", "haha");
 	printf("Return value = %d\n", rv); 
