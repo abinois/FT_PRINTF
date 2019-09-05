f63dadcdd49409714791b4010cdc49bf70984c68
ARTHUR BINOIS <arthurbinois@MacBook-Pro-de-ARTHUR.local>
2019-09-05 23:39:36 +0200

    + italic pink purple orange overline

--- color.c
+++ color.c
@@ -39,32 +39,44 @@ void	fill_colors(char **color)
 	color[8] = "{reset}";
 	color[9] = "{rev}";
 	color[10] = "{blink}";
-	color[11] = "error";
-	color[12] = "\033[31m";
-	color[13] = "\033[32m";
-	color[14] = "\033[33m";
-	color[15] = "\033[34m";
-	color[16] = "\033[35m";
-	color[17] = "\033[36m";
-	color[18] = "\033[30m";
-	color[19] = "\033[4m";
-	color[20] = "\033[0m";
-	color[21] = "\033[7m";
-	color[22] = "\033[5m";
+	color[11] = "{bold}";
+	color[12] = "{over}";
+	color[13] = "{italic}";
+	color[14] = "{orange}";
+	color[15] = "{purple}";
+	color[16] = "{pink}";
+	color[17] = "error";
+	color[18] = "\033[31m";
+	color[19] = "\033[32m";
+	color[20] = "\033[33m";
+	color[21] = "\033[34m";
+	color[22] = "\033[35m";
+	color[23] = "\033[36m";
+	color[24] = "\033[30m";
+	color[25] = "\033[4m";
+	color[26] = "\033[0m";
+	color[27] = "\033[7m";
+	color[28] = "\033[5m";
+	color[29] = "\033[1m";
+	color[30] = "\033[9m";
+	color[31] = "\033[3m";
+	color[32] = "\033[38;5;208m";
+	color[33] = "\033[38;5;129m";
+	color[34] = "\033[38;5;206m";
 }
 
 int		what_color(char *buf, int i)
 {
 	int		x;
 	int		a;
-	char	*color[23];
+	char	*color[35];
 
 	fill_colors(color);
 	x = i;
 	a = 0;
-	while (a <= 10 && ft_strncmp(color[a], buf + x, ft_strlen(color[a])))
+	while (a <= 16 && ft_strncmp(color[a], buf + x, ft_strlen(color[a])))
 		a++;
-	return (a == 11 ? -1 : a);
+	return (a == 17 ? -1 : a);
 }
 
 int		return_value(char *buf)
@@ -76,16 +88,19 @@ int		return_value(char *buf)
 	x = 0;
 	i = -1;
 	while (buf[++i])
-	{
 		if (buf[i] == '{' && (c = what_color(buf, i)) >= 0)
 		{
-			x += c < 7 ? 5 : 4;
+			if (c < 7)
+				x += 5;
+			else if (c < 14)
+				x += 4;
+			else if (c < 17)
+				x += 11;
 			while (buf[i] != '}')
 				i++;
 		}
 		else
 			x++;
-	}
 	return (x);
 }
 
@@ -93,7 +108,7 @@ int		put_color(char *buf, int x, int i)
 {
 	int		k;
 	int		c;
-	char	*color[23];
+	char	*color[35];
 
 	fill_colors(color);
 	if (g_fd < 0 || g_fd > OPEN_MAX)
@@ -108,7 +123,7 @@ int		put_color(char *buf, int x, int i)
 		i += k;
 		if (i < x && buf[i] == '{' && (c = what_color(buf, i)) >= 0)
 		{
-			ft_putstr_fd(color[c + 12], g_fd);
+			ft_putstr_fd(color[c + 18], g_fd);
 			while (buf[i] != '}')
 				i++;
 		}
